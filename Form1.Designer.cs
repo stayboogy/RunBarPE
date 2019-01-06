8cf72d1bd8baf0ac27f272681bd1494ee31f5690
unknown <idaboogerman@gmail.com>
2019-01-05 22:13:08 -0600

    upload actual

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RunBarPE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd = new System.Windows.Forms.Button();
+            this.taskmgr = new System.Windows.Forms.Button();
+            this.regedit = new System.Windows.Forms.Button();
+            this.notepad = new System.Windows.Forms.Button();
+            this.bootice = new System.Windows.Forms.Button();
+            this.gimagex = new System.Windows.Forms.Button();
+            this.qdir = new System.Windows.Forms.Button();
+            this.finder = new System.Windows.Forms.Button();
+            this.fastcopy = new System.Windows.Forms.Button();
+            this.zip = new System.Windows.Forms.Button();
+            this.nsudo = new System.Windows.Forms.Button();
+            this.shutdown = new System.Windows.Forms.Button();
+            this.reboot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmd
+            // 
+            this.cmd.Location = new System.Drawing.Point(12, 12);
+            this.cmd.Name = "cmd";
+            this.cmd.Size = new System.Drawing.Size(75, 23);
+            this.cmd.TabIndex = 0;
+            this.cmd.Text = "CMD";
+            this.cmd.UseVisualStyleBackColor = true;
+            this.cmd.Click += new System.EventHandler(this.cmd_Click);
+            // 
+            // taskmgr
+            // 
+            this.taskmgr.Location = new System.Drawing.Point(12, 41);
+            this.taskmgr.Name = "taskmgr";
+            this.taskmgr.Size = new System.Drawing.Size(75, 23);
+            this.taskmgr.TabIndex = 1;
+            this.taskmgr.Text = "TASKMGR";
+            this.taskmgr.UseVisualStyleBackColor = true;
+            this.taskmgr.Click += new System.EventHandler(this.taskmgr_Click);
+            // 
+            // regedit
+            // 
+            this.regedit.Location = new System.Drawing.Point(12, 70);
+            this.regedit.Name = "regedit";
+            this.regedit.Size = new System.Drawing.Size(75, 23);
+            this.regedit.TabIndex = 2;
+            this.regedit.Text = "REGEDIT";
+            this.regedit.UseVisualStyleBackColor = true;
+            this.regedit.Click += new System.EventHandler(this.regedit_Click);
+            // 
+            // notepad
+            // 
+            this.notepad.Location = new System.Drawing.Point(12, 99);
+            this.notepad.Name = "notepad";
+            this.notepad.Size = new System.Drawing.Size(75, 23);
+            this.notepad.TabIndex = 3;
+            this.notepad.Text = "NOTEPAD";
+            this.notepad.UseVisualStyleBackColor = true;
+            this.notepad.Click += new System.EventHandler(this.notepad_Click);
+            // 
+            // bootice
+            // 
+            this.bootice.Location = new System.Drawing.Point(12, 128);
+            this.bootice.Name = "bootice";
+            this.bootice.Size = new System.Drawing.Size(75, 23);
+            this.bootice.TabIndex = 4;
+            this.bootice.Text = "BOOTICE";
+            this.bootice.UseVisualStyleBackColor = true;
+            this.bootice.Click += new System.EventHandler(this.bootice_Click);
+            // 
+            // gimagex
+            // 
+            this.gimagex.Location = new System.Drawing.Point(12, 157);
+            this.gimagex.Name = "gimagex";
+            this.gimagex.Size = new System.Drawing.Size(75, 23);
+            this.gimagex.TabIndex = 5;
+            this.gimagex.Text = "GIMAGEX";
+            this.gimagex.UseVisualStyleBackColor = true;
+            this.gimagex.Click += new System.EventHandler(this.gimagex_Click);
+            // 
+            // qdir
+            // 
+            this.qdir.Location = new System.Drawing.Point(12, 186);
+            this.qdir.Name = "qdir";
+            this.qdir.Size = new System.Drawing.Size(75, 23);
+            this.qdir.TabIndex = 6;
+            this.qdir.Text = "Q-DIR";
+            this.qdir.UseVisualStyleBackColor = true;
+            this.qdir.Click += new System.EventHandler(this.qdir_Click);
+            // 
+            // finder
+            // 
+            this.finder.Location = new System.Drawing.Point(12, 215);
+            this.finder.Name = "finder";
+            this.finder.Size = new System.Drawing.Size(75, 23);
+            this.finder.TabIndex = 7;
+            this.finder.Text = "FINDER";
+            this.finder.UseVisualStyleBackColor = true;
+            this.finder.Click += new System.EventHandler(this.finder_Click);
+            // 
+            // fastcopy
+            // 
+            this.fastcopy.Location = new System.Drawing.Point(12, 244);
+            this.fastcopy.Name = "fastcopy";
+            this.fastcopy.Size = new System.Drawing.Size(75, 23);
+            this.fastcopy.TabIndex = 8;
+            this.fastcopy.Text = "FAST COPY";
+            this.fastcopy.UseVisualStyleBackColor = true;
+            this.fastcopy.Click += new System.EventHandler(this.fastcopy_Click);
+            // 
+            // zip
+            // 
+            this.zip.Location = new System.Drawing.Point(12, 273);
+            this.zip.Name = "zip";
+            this.zip.Size = new System.Drawing.Size(75, 23);
+            this.zip.TabIndex = 9;
+            this.zip.Text = "7-ZIP";
+            this.zip.UseVisualStyleBackColor = true;
+            this.zip.Click += new System.EventHandler(this.zip_Click);
+            // 
+            // nsudo
+            // 
+            this.nsudo.Location = new System.Drawing.Point(12, 302);
+            this.nsudo.Name = "nsudo";
+            this.nsudo.Size = new System.Drawing.Size(75, 23);
+            this.nsudo.TabIndex = 10;
+            this.nsudo.Text = "NSUDO";
+            this.nsudo.UseVisualStyleBackColor = true;
+            this.nsudo.Click += new System.EventHandler(this.nsudo_Click);
+            // 
+            // shutdown
+            // 
+            this.shutdown.Location = new System.Drawing.Point(12, 360);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(75, 23);
+            this.shutdown.TabIndex = 11;
+            this.shutdown.Text = "HALT";
+            this.shutdown.UseVisualStyleBackColor = true;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // reboot
+            // 
+            this.reboot.Location = new System.Drawing.Point(12, 331);
+            this.reboot.Name = "reboot";
+            this.reboot.Size = new System.Drawing.Size(75, 23);
+            this.reboot.TabIndex = 12;
+            this.reboot.Text = "REBOOT";
+            this.reboot.UseVisualStyleBackColor = true;
+            this.reboot.Click += new System.EventHandler(this.reboot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(103, 394);
+            this.Controls.Add(this.reboot);
+            this.Controls.Add(this.shutdown);
+            this.Controls.Add(this.nsudo);
+            this.Controls.Add(this.zip);
+            this.Controls.Add(this.fastcopy);
+            this.Controls.Add(this.finder);
+            this.Controls.Add(this.qdir);
+            this.Controls.Add(this.gimagex);
+            this.Controls.Add(this.bootice);
+            this.Controls.Add(this.notepad);
+            this.Controls.Add(this.regedit);
+            this.Controls.Add(this.taskmgr);
+            this.Controls.Add(this.cmd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RunBarPE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd;
+        private System.Windows.Forms.Button taskmgr;
+        private System.Windows.Forms.Button regedit;
+        private System.Windows.Forms.Button notepad;
+        private System.Windows.Forms.Button bootice;
+        private System.Windows.Forms.Button gimagex;
+        private System.Windows.Forms.Button qdir;
+        private System.Windows.Forms.Button finder;
+        private System.Windows.Forms.Button fastcopy;
+        private System.Windows.Forms.Button zip;
+        private System.Windows.Forms.Button nsudo;
+        private System.Windows.Forms.Button shutdown;
+        private System.Windows.Forms.Button reboot;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunBarPE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmd_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"cmd.exe");
+        }
+
+        private void taskmgr_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"taskmgr.exe");
+        }
+
+        private void regedit_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"regedit.exe");
+        }
+
+        private void notepad_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\note.exe");
+
+        }
+
+        private void bootice_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\bootice.exe");
+        }
+
+        private void gimagex_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\gimagex.exe");
+        }
+
+        private void qdir_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\Q-Dir\Q-Dir.exe");
+        }
+
+        private void finder_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\find.exe");
+        }
+
+        private void fastcopy_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\FastCopy\FastCopy.exe");
+        }
+
+        private void zip_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\7-Zip\7zFM.exe");
+        }
+
+        private void nsudo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"X:\Tools\NSudo\NSudo.bat");
+        }
+
+        private void reboot_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo rebootP = new System.Diagnostics.ProcessStartInfo();
+            rebootP.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            rebootP.FileName = "wpeutil.exe";
+            rebootP.Arguments = "Reboot";
+            System.Diagnostics.Process.Start(rebootP);
+        }
+
+        private void shutdown_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo shutdownP = new System.Diagnostics.ProcessStartInfo();
+            shutdownP.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            shutdownP.FileName = "wpeutil.exe";
+            shutdownP.Arguments = "Shutdown";
+            System.Diagnostics.Process.Start(shutdownP);
+        }
+    }
+}
